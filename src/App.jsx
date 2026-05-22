cad93b748cc2cd1eb7fe8803bd75a6dc2aa96a55
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-05-22 21:39:32 +0530

    fix: remove .jsx extension from imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // import Login from "./userAuth/login";
+// // import Register from "./userAuth/register";
+// import EmployeeDashboard from "./pages/EmployeeComponent/EmployeeDashBoard";
+// import "./App.css";
+// import MyTasks from "./pages/EmployeeComponent/MyTasks";
+// import EmployeeLayout from "./pages/EmployeeComponent/EmployeeLayOut";
+
+// import OneTask from "./pages/tlComponent/TLtask";
+// import { ToastContainer } from "react-toastify";
+// import MyTeam from "./pages/tlComponent/MyTeam";
+// import CreateTask from "./pages/tlComponent/createTask";
+// import CreateEmployee from "./pages/tlComponent/createEmployee";
 // import Login from "./userAuth/login";
 // import Register from "./userAuth/register";
+// import TLDashboard from "./pages/tlComponent/TLdashboard.jsx";
+// import TLLayout from "./pages/tlComponent/TLlayout.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import EmployeeDashboard from "./pages/EmployeeComponent/EmployeeDashBoard";
-import "./App.css";
-import MyTasks from "./pages/EmployeeComponent/MyTasks";
 import EmployeeLayout from "./pages/EmployeeComponent/EmployeeLayOut";
+import MyTasks from "./pages/EmployeeComponent/MyTasks";
 
 import OneTask from "./pages/tlComponent/TLtask";
-import { ToastContainer } from "react-toastify";
-// import TLLayout from "./pages/tlComponent/TLlayout";
-// import TLDashboard from "./pages/tlComponent/TLdashboard";
 import MyTeam from "./pages/tlComponent/MyTeam";
 import CreateTask from "./pages/tlComponent/createTask";
 import CreateEmployee from "./pages/tlComponent/createEmployee";
+
+import TLDashboard from "./pages/tlComponent/TLdashboard";
+import TLLayout from "./pages/tlComponent/TLlayout";
+
 import Login from "./userAuth/login";
 import Register from "./userAuth/register";
-// import TLLayout from "./pages/tlComponent/TLlayout";
-import TLDashboard from "./pages/tlComponent/TLdashboard.jsx";
-import TLLayout from "./pages/tlComponent/TLlayout.jsx";
+
+import { ToastContainer } from "react-toastify";
+import "./App.css";
 
 // Pages
 
