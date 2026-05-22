f5b865c465084131181866f4aa0d07623233bc53
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-05-22 11:01:10 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./userAuth/login";
-import Register from "./userAuth/register";
+// import Login from "./userAuth/login";
+// import Register from "./userAuth/register";
 import EmployeeDashboard from "./pages/EmployeeComponent/EmployeeDashBoard";
 import "./App.css";
 import MyTasks from "./pages/EmployeeComponent/MyTasks";
 import EmployeeLayout from "./pages/EmployeeComponent/EmployeeLayOut";
-import TLLayout from "./pages/tlComponent/TLLayout";
-import CreateEmployee from "./pages/tlComponent/createEmployee";
-import TLDashboard from "./pages/tlComponent/TLdashboard";
-import MyTeam from "./pages/tlComponent/MyTeam";
-import CreateTask from "./pages/tlComponent/createTask";
+// import TLLayout from "./pages/tlComponent/TLLayout";
+// import CreateEmployee from "./pages/tlComponent/createEmployee";
+// import TLDashboard from "./pages/tlComponent/TLdashboard";
+// import MyTeam from "./pages/tlComponent/MyTeam";
+// import CreateTask from "./pages/tlComponent/createTask";
 import OneTask from "./pages/tlComponent/TLtask";
 import { ToastContainer } from "react-toastify";
+import TLLayout from "./pages/tlComponent/TLlayOut";
+import TLDashboard from "./pages/tlComponent/TLdashBoard";
+import MyTeam from "./pages/tlComponent/MyTeam";
+import CreateTask from "./pages/tlComponent/createTask";
+import CreateEmployee from "./pages/tlComponent/createEmployee";
+import Login from "./userAuth/login";
+import Register from "./userAuth/register";
 
 // Pages
 
@@ -23,7 +30,7 @@ function App() {
       <Routes>
         {/* Login */}
 
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login/>} />
         <Route path="/login" element={<Login />} />
 
         {/* Register */}
@@ -38,7 +45,7 @@ function App() {
 
         <Route element={<TLLayout />}>
           <Route path="/tl-dashboard" element={<TLDashboard />} />
-          <Route path="/my-team" element={<MyTeam />} />
+          <Route path="/my-team" element={<MyTeam/>} />
 
           <Route path="/create-task" element={<CreateTask />} />
 
